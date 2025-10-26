56c4418eae0bb7c23fe0010e5c4803d855e2bb89
ME <me@192.168.1.200>
2025-10-27 03:15:49 +0700

    12 commit

--- task/management/commands/check_smtp.py
+++ task/management/commands/check_smtp.py
@@ -1,16 +1,22 @@
 from django.core.management.base import BaseCommand
-from resend.client import Resend  # correct import
-from django.conf import settings
+import os
+import resend
 
 class Command(BaseCommand):
     help = 'Send a test email via Resend'
 
     def handle(self, *args, **kwargs):
-        client = Resend(api_key=settings.RESEND_API_KEY)
-        client.emails.send(
-            from_="dyper777@gmail.com",
-            to=["dyper777@gmail.com"],
-            subject="Hello from Railway",
-            html="<p>This email works!</p>",
-        )
-        self.stdout.write(self.style.SUCCESS('Email sent!'))
+        # Set your API key
+        resend.api_key = os.environ.get("RESEND_API_KEY")
+
+        # Prepare email params
+        params: resend.Emails.SendParams = {
+            "from": "dyper777@gmail.com",
+            "to": ["dyper777@gmail.com"],
+            "subject": "Hello from Railway",
+            "html": "<p>This email works!</p>",
+        }
+
+        # Send the email
+        email = resend.Emails.send(params)
+        self.stdout.write(self.style.SUCCESS(f'Email sent! ID: {email.id}'))
