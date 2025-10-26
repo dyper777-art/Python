2e334bd7f7b89c11b1d61bfa9554466cb31f7948
ME <me@192.168.1.200>
2025-10-27 03:09:54 +0700

    eleventh commit

--- task/management/commands/check_smtp.py
+++ task/management/commands/check_smtp.py
@@ -1,12 +1,12 @@
 from django.core.management.base import BaseCommand
-from resend import ResendClient  # updated import
+from resend import Resend
 from django.conf import settings
 
 class Command(BaseCommand):
     help = 'Send a test email via Resend'
 
     def handle(self, *args, **kwargs):
-        client = ResendClient(api_key=settings.RESEND_API_KEY)  # updated initialization
+        client = Resend(settings.RESEND_API_KEY)
         client.emails.send(
             from_="dyper777@gmail.com",
             to=["dyper777@gmail.com"],
